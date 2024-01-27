65b9766c1add4128e94c37bfa67e7801f3de89c1
Marianna <marry-cherry-pie@mail.ru>
2024-01-27 19:55:50 +0200

    Добавляет поддержку dotenv и класс RestConfig

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,16 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config } from '../shared/libs/config/index.js';
 
 
 export class RestApplication {
   constructor(
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config
   ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from $PORT: ${this.config.get('PORT')}`);
+    this.logger.info(`Get value from $MY VALUE: ${this.config.get('MY_VALUE')}`);
   }
 }
