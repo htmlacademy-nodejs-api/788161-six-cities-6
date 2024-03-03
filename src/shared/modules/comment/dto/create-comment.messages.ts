c2353c48868691bca0f1561bb1ea1f94c1cfbedd
Marianna <marry-cherry-pie@mail.ru>
2024-03-03 22:42:54 +0200

    Вносит вторую часть правок для получения допуска

--- src/shared/modules/comment/dto/create-comment.messages.ts
+++ src/shared/modules/comment/dto/create-comment.messages.ts
@@ -3,7 +3,7 @@ import { RATING, TEXT_LENGTH } from '../comment.constant.js';
 export const CREATE_COMMENT_MESSAGES = {
   TEXT: {
     INVALID_FORMAT: 'Text is required',
-    LENGTH_FIELD: `Min length is ${TEXT_LENGTH.MIN}, max is ${TEXT_LENGTH.MIN}`
+    LENGTH_FIELD: `Min length is ${TEXT_LENGTH.MIN}, max is ${TEXT_LENGTH.MAX}`
   },
   RATING: {
     INVALID_FORMAT: 'Rating field must be an integer',
